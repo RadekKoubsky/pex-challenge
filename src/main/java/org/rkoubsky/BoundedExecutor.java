2294b1a472ee93f1ba1637d9ded973daf60fa1a6
Radek Koubsky <radekkoubsky@gmail.com>
2019-12-04 22:03:05 +0100

    Encapsulate executor service with bounded executor.

--- src/main/java/org/rkoubsky/BoundedExecutor.java
+++ src/main/java/org/rkoubsky/BoundedExecutor.java
@@ -1,21 +1,23 @@
 package org.rkoubsky;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class BoundedExecutor {
     private final ExecutorService exec;
     private final Semaphore semaphore;
 
-    public BoundedExecutor(final ExecutorService exec, final int bound) {
-        this.exec = exec;
+    public BoundedExecutor(final int nProducers, final int tasks) {
+        this.exec = Executors.newFixedThreadPool(nProducers);
         /**
          * set the bound on the semaphore to be equal to
          * the pool size plus the number of queued tasks you want to allow
          */
-        this.semaphore = new Semaphore(bound);
+        this.semaphore = new Semaphore(nProducers + tasks);
     }
 
     public void submitTask(final Runnable command) throws InterruptedException {
@@ -34,6 +36,11 @@ public class BoundedExecutor {
         } catch (final RejectedExecutionException e) {
             this.semaphore.release();
         }
+    }
+
+    public int activeThreads(){
+        final ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) this.exec;
+        return threadPoolExecutor.getActiveCount();
     }
 
     public void stop() throws InterruptedException {
--- src/main/java/org/rkoubsky/PexChallenge.java
+++ src/main/java/org/rkoubsky/PexChallenge.java
@@ -2,10 +2,7 @@ package org.rkoubsky;
 
 import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class PexChallenge {
@@ -15,8 +12,7 @@ public class PexChallenge {
 
     public static void main(final String[] args) throws IOException, InterruptedException {
         final String fileName = args[0];
-        final ExecutorService exec = Executors.newFixedThreadPool(N_PRODUCERS);
-        final BoundedExecutor boundedExec = new BoundedExecutor(exec, N_PRODUCERS + MAX_TASKS);
+        final BoundedExecutor boundedExec = new BoundedExecutor(N_PRODUCERS, MAX_TASKS);
         final BlockingQueue<PrevalentColors> imageQueue = new LinkedBlockingQueue<>();
 
         final ImagesService imagesService = new ImagesService(boundedExec, imageQueue);
@@ -26,7 +22,7 @@ public class PexChallenge {
         final long start = System.nanoTime();
         writer.start();
         imagesService.process(fileName);
-        waitForFinish(exec);
+        waitForFinish(boundedExec);
         final long elapsedTime = System.nanoTime() - start;
         System.out.printf("Processing %s images has finished, took %s s\n", writerTask.getProcessedImages().longValue(),
                           TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS));
@@ -34,9 +30,8 @@ public class PexChallenge {
         writer.interrupt();
     }
 
-    private static void waitForFinish(final ExecutorService exec) throws InterruptedException {
-        final ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) exec;
-        while (threadPoolExecutor.getActiveCount() > 0){
+    private static void waitForFinish(final BoundedExecutor exec) throws InterruptedException {
+        while (exec.activeThreads() > 0){
             TimeUnit.SECONDS.sleep(3);
         }
     }
